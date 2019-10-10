3896cd6384d97b293b00fb25d01e7d4ec92de78e
Tina <longevity.tina@gmail.com>
2019-10-10 11:14:51 -0700

    Adding final score box

--- pigGame/appChallenge.js
+++ pigGame/appChallenge.js
@@ -110,7 +110,7 @@ function nextPlayer(){
 
         document.getElementById('current-0').textContent = '0';
         document.getElementById('current-1').textContent = '0';
-        debugger
+        
         document.querySelector('.player-1-panel').classList.toggle('active');
         document.querySelector('.player-0-panel').classList.toggle('active');
 
